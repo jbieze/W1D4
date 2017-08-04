252be067c054a3a14f1e3a8d7872a21494e5585e
App Academy Student <student@appacademy.io>
2017-08-03 18:06:21 -0700

    Sweep mines

---
+++ tile.rb
@@ -0,0 +1,27 @@
+class Tile
+
+
+# on initialize
+  - value (from file)
+  - flagged?(defaul to false)
+  - revealed? (default to false)
+  - (account for display of default char L)
+
+  file needs:
+  ---------
+
+# def by_bomb(pos)
+# - check spots in each direction (including diagonals)
+# - if spot contains bomb, += 1
+# - display sum on pos
+
+
+
+
+
+
+
+
+#
+#
+end
